14137438330ae8b48fd8e0a52be3e80bee246e77
Kasper Kristensen <kaskr@dtu.dk>
2017-06-30 16:11:44 +0200

    Preliminary plots

---
+++ calib/calib.R
@@ -0,0 +1,116 @@
+## ========================== OLD GEAR CALIB DATA
+
+d <- read.table("calib_old_gear.csv",sep=",",header=TRUE, check.names = FALSE)
+d <- d[-(9:10)] ## Remove subsampling info
+
+## Reshape dataset
+
+## Extract diver data
+head(d)
+d$is_kg <- as.character(d$Station) %in% LETTERS[1:10]
+d1 <- with(d, data.frame(
+                  Total = `Total (g)` / ifelse(is_kg, 1, 1000),
+                  BMS = `BMS (g)` / ifelse(is_kg, 1, 1000),
+                  Station = Station,
+                  Area = `Ring area`
+              )
+           )
+d1$Dredge <- "outside"
+
+## Extract dredge data
+head(d)
+d2 <- with(d, data.frame(
+                  Total = `Total fangst (kg)`,
+                  BMS = `BMS (kg)`,
+                  Station = Station,
+                  Area = `Area`
+              )
+           )
+d2$Dredge <- "inside"
+d2 <- subset(d2, !is.na(Total))
+
+dd <- rbind(d1, d2)
+dd$Gear <- "old"
+
+## ========================== NEW GEAR CALIB DATA
+
+d <- read.csv("calib_new_gear.csv", header=TRUE, check.names=FALSE, na.strings = "na")
+names(d)[3] <- "Pos_within_dredge"
+d$Rectangle <- factor(1:nrow(d))
+dnew <- do.call("rbind", rep(list(d[-(4:7)]), 4))
+dnew$haul.id <- factor(paste(dnew$Area, dnew$Dredge))
+v <- do.call("c", d[4:7])
+dnew$box <- substring(names(v), 1,1)
+dnew$inside <- sub("\\).*","",sub(".*\\(","",names(v))) == "inside"
+dnew$outside <- sub("\\).*","",sub(".*\\(","",names(v))) == "outside"
+dnew$weight <- v
+dnew <- na.omit(dnew)
+names(dnew) <- sub(" .*", "", names(dnew))
+dnew[1:6] <- lapply(dnew[1:6], factor)
+
+d3 <- with(dnew, data.frame(
+                     Total = NA, ## FIXME !!!!
+                     BMS = `weight` / 1000,
+                     Station = haul.id,
+                     Area = 0.25,
+                     Dredge = ifelse(inside, "inside", "outside")
+                 )
+           )
+
+d3$Gear <- "new"
+
+dd <- rbind(dd, d3)
+
+
+## library(glmmTMB)
+## fit <- glmmTMB( Total ~ Station + offset(log(Area)) + Gear - 1, data=dd, family=poisson(link="log") )
+
+y <- dd$Total / dd$Area
+tab <- tapply(y, list(dd$Dredge, dd$Station), mean, na.rm=TRUE)
+df1 <- as.data.frame(t(tab))
+plot(df1)
+
+y <- dd$BMS / dd$Area
+tab <- tapply(y, list(dd$Dredge, dd$Station), mean, na.rm=TRUE)
+df2 <- as.data.frame(t(tab))
+points(df2,col="red")
+
+df <- rbind(df1,df2)
+
+plot(sqrt(df), pch=16)
+points(sqrt(df2), col="red" , pch=16)
+
+
+plot(log(df), pch=16)
+points(log(df2), col="red" , pch=16)
+
+
+summary(fm0 <- lm(outside~inside-1,data=df))
+summary(fm1 <- lm(log(outside)~log(inside),data=df))
+summary(fm2 <- lm(sqrt(outside)~sqrt(inside),data=df))
+
+f0 <- function(x)coef(fm0) * x
+f1 <- function(x)exp(coef(fm1)[1]) * x^coef(fm1)[2]
+f2 <- function(x)coef(fm2)[2]*x+ coef(fm2)[1] +2*prod(coef(fm2))*sqrt(x)
+f3 <- function(x)2.703*x^0.29
+plot(f0,0,15)
+plot(f1,0,15,add=TRUE,col=2)
+plot(f2,0,15,add=TRUE,col=3)
+plot(f3,0,15,add=TRUE,col=4)
+## abline(v=range(df$inside, na.rm=TRUE))
+## points(df, pch=16)
+points(df2, pch=16, col=grepl(" ", rownames(df))+1)
+
+
+## Plot in sqrt-transformed domain
+tplot <- function(f,a,b,...){plot(function(x)sqrt(f(x^2)), sqrt(a), sqrt(b), ..., xlab="sqrt(x)", ylab="sqrt(y)")}
+tplot(f0,0,15)
+tplot(f1,0,15,add=TRUE,col=2)
+tplot(f2,0,15,add=TRUE,col=3)
+tplot(f3,0,15,add=TRUE,col=4)
+## abline(v=range(df$inside, na.rm=TRUE))
+##points(sqrt(df), pch=16)
+##points(sqrt(df2), pch=16, col="red")
+points(sqrt(df), pch=16, col=grepl(" ", rownames(df))+1)
+
+
